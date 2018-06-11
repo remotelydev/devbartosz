70bd2dcc61e640fef4432fc263f5028ad7b00baa
Bartosz <trzosoo@gmail.com>
2018-06-11 15:20:48 +0100

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <nuxt/>
+  </div>
+</template>
+
+
+<style lang="scss">
+@import '~/scss/vars.scss';
+
+body {
+  background: #101010;
+  color: #EFEFEF;
+}
+
+</style>
+
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="home">
+      <h1>Bartosz Trzos</h1>
+      <h2>Javascript Engineer</h2>
+  </section>
+</template>
+
+<script>
+export default {
+  components: {
+  }
+}
+</script>
+<style lang="scss" scoped>
+.home {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  width: 100vw;
+  height: 100vh;
+}
+</style>
+
