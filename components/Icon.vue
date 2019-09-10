c7f95a58f9a5fb7007ea7f9ea79040372a3f73aa
Bartosz <trzosoo@gmail.com>
2019-09-10 16:51:00 +0200

    Update content, layout, colors and icons

---
+++ components/Icon.vue
@@ -0,0 +1,25 @@
+<template>
+  <svg class="icon">
+    <use :xlink:href="`#icon-${icon}`" />
+  </svg>
+</template>
+
+<script>
+export default {
+  props: {
+    icon: {
+      type: String,
+      default: ""
+    }
+  }
+};
+</script>
+
+<style>
+svg {
+  max-height: 1em;
+  max-width: 1em;
+  color: inherit;
+  fill: currentcolor;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,47 @@
 <template>
   <section class="home">
-      <h1>Bartosz Trzos</h1>
-      <h2>Javascript Engineer</h2>
+    <h1>Bartosz Trzos</h1>
+    <p>Progressive JavaScript engineer, with passion for UI and UX of web applications. Fast thinker and learner with a deep understanding of frontend side of website creation. Usually, work with Vue and React.</p>
+    <div class="social">
+      <a href="https://twitter.com/devbartosz">
+        <Icon icon="twitter" />
+      </a>
+      <a href="https://github.com/Trzosoo">
+        <Icon icon="github" />
+      </a>
+      <a href="https://www.linkedin.com/in/bartosz-trzos/">
+        <Icon icon="linkedin" />
+      </a>
+    </div>
   </section>
 </template>
 
 <script>
+import Icon from "../components/Icon";
+
 export default {
   components: {
+    Icon
   }
-}
+};
 </script>
 <style lang="scss" scoped>
-.home {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
+@import "~/scss/vars.scss";
 
-  width: 100vw;
-  height: 100vh;
+.home {
+  max-width: 20rem;
+  padding: 1rem;
+}
+.social {
+  display: flex;
+  justify-content: space-between;
+  font-size: 2rem;
+  a {
+    color: $secondary;
+    &:hover {
+      color: darken($secondary, 10);
+    }
+  }
 }
 </style>
 
